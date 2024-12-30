9a8e52d1d4c7d8411b178ced3bb9dc45d005d19e
Katrina <katrinabyrd88@gmail.com>
2024-12-30 03:00:06 -0500

    added parameterized queries to students.js and teachers.js queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,7 +8,7 @@ const pool = new Pool({
 });
 
 const cohortName = process.argv[2];
-const limit = process.argv[3] || 5;
+const limit = process.argv[3];
 
 const queryString = `
   SELECT students.id, students.name, cohorts.name as cohort
@@ -18,12 +18,5 @@ const queryString = `
   LIMIT $2;
 `;
 const values = [`%${cohortName}%`, limit];
+pool.query(queryString, values);
 
-pool
-  .query(queryString, values)
-  .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.id} and was in cohort ${user.cohort}`);
-    });
-  })
-  .catch(err => console.error('query error', err.stack));
